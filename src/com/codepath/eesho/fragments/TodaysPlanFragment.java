e556436fa5d5a7775353091149bd861297596e91
caren <cchang@tagged.com>
2014-07-07 20:01:41 -0700

    fresh commit with everything

---
+++ src/com/codepath/eesho/fragments/TodaysPlanFragment.java
@@ -0,0 +1,51 @@
+package com.codepath.eesho.fragments;
+
+import java.util.ArrayList;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.codepath.eesho.R;
+import com.codepath.eesho.adapters.GoalArrayAdapter;
+import com.codepath.eesho.models.Goal;
+
+public class TodaysPlanFragment extends Fragment{
+
+	private ArrayList<Goal> goals;
+	private GoalArrayAdapter aGoals;
+	ListView lvGoals;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_goals_list, container, false);
+		lvGoals = (ListView) v.findViewById(R.id.lvGoals);
+		lvGoals.setAdapter(aGoals);
+		
+		return v;
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		goals = new ArrayList<Goal>();
+		aGoals = new GoalArrayAdapter(getActivity(), goals);
+		goals.add(new Goal(true, "eat mcdonalds"));
+		goals.add(new Goal(false, "don't eat mcdonalds"));
+		
+//		addAll(goals);
+	}
+	
+	public void addAll(ArrayList<Goal> goals) {
+		aGoals.addAll(goals);
+	}
+	
+}
