4cd7e2a2ffa1548d0a969b29fb2a297f31332d6c
caren <cchang@tagged.com>
2014-07-10 00:59:49 -0700

    hooking up dailyplanfragment to parse data

--- src/com/codepath/eesho/fragments/TodaysPlanFragment.java
+++ src/com/codepath/eesho/fragments/TodaysPlanFragment.java
@@ -1,19 +1,27 @@
 package com.codepath.eesho.fragments;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+
 import com.codepath.eesho.R;
 import com.codepath.eesho.adapters.GoalArrayAdapter;
 import com.codepath.eesho.models.Goal;
+import com.codepath.eesho.models.Plan;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 
 @SuppressLint("ValidFragment")
 public class TodaysPlanFragment extends Fragment{
@@ -35,24 +43,85 @@ public class TodaysPlanFragment extends Fragment{
 		lvGoals.setAdapter(aGoals);
 		
 		return v;
+		
 	}
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		ParseQuery<Plan> query = ParseQuery.getQuery(Plan.class);
+		// Define our query conditions
+		query.whereEqualTo("username", "calren");
 				
 		goals = new ArrayList<Goal>();
 		aGoals = new GoalArrayAdapter(getActivity(), goals);
 		if (day == 1) {
-			goals.add(new Goal(true, "eat mcdonalds"));
-			goals.add(new Goal(false, "don't eat mcdonalds"));
+			query.findInBackground(new FindCallback<Plan>() {
+			    public void done(List<Plan> plan, ParseException e) {
+			        if (e == null) {
+			            // Access the array of results here
+			            JSONArray first = plan.get(0).getTodaysGoals();
+			            for (int i = 0; i < first.length(); i++) {
+			            	try {
+			            		goals.add(new Goal(first.getJSONObject(i).get("done").toString(),
+			            				first.getJSONObject(i).get("task").toString()));
+							} catch (JSONException e1) {
+								e1.printStackTrace();
+							}
+			            }
+			            aGoals.notifyDataSetChanged();
+			            
+			        } else {
+			            Log.d("item", "Error: " + e.getMessage());
+			        }
+			    }
+			});
+
 		} else if (day == 0) {
-			goals.add(new Goal(false, "I failed"));
+			query.findInBackground(new FindCallback<Plan>() {
+			    public void done(List<Plan> plan, ParseException e) {
+			        if (e == null) {
+			            // Access the array of results here
+			            JSONArray first = plan.get(0).getYesterdayGoals();
+			            for (int i = 0; i < first.length(); i++) {
+			            	try {
+			            		goals.add(new Goal(first.getJSONObject(i).get("done").toString(),
+			            				first.getJSONObject(i).get("task").toString()));
+							} catch (JSONException e1) {
+								e1.printStackTrace();
+							}
+			            }
+			            aGoals.notifyDataSetChanged();
+			            
+			        } else {
+			            Log.d("item", "Error: " + e.getMessage());
+			        }
+			    }
+			});
 		} else if (day == 2) {
-			goals.add(new Goal(false, "relax"));
+			query.findInBackground(new FindCallback<Plan>() {
+			    public void done(List<Plan> plan, ParseException e) {
+			        if (e == null) {
+			            // Access the array of results here
+			            JSONArray first = plan.get(0).getTomorrowsGoals();
+			            for (int i = 0; i < first.length(); i++) {
+			            	try {
+			            		goals.add(new Goal(first.getJSONObject(i).get("done").toString(),
+			            				first.getJSONObject(i).get("task").toString()));
+							} catch (JSONException e1) {
+								e1.printStackTrace();
+							}
+			            }
+			            aGoals.notifyDataSetChanged();
+			            
+			        } else {
+			            Log.d("item", "Error: " + e.getMessage());
+			        }
+			    }
+			});
 		}
 		
-//		addAll(goals);
 	}
 	
 	public void addAll(ArrayList<Goal> goals) {
--- src/com/codepath/eesho/models/Goal.java
+++ src/com/codepath/eesho/models/Goal.java
@@ -1,13 +1,33 @@
 package com.codepath.eesho.models;
 
+import java.util.List;
+
+import org.json.JSONArray;
+
+import android.util.Log;
+
+import com.parse.FindCallback;
 import com.parse.ParseClassName;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 @ParseClassName("Goal")
 public class Goal extends ParseObject{
 	
 	public Goal() {
 		super();
+	}
+	
+	public Goal(String isDone, String desc) {
+		super();
+		if (isDone.contains("true")) {
+			setIsDone(true);
+		} else {
+			setIsDone(false);
+		}
+		setDescription(desc);
 	}
 	
 	public Goal(boolean isDone, String desc) {
@@ -35,5 +55,4 @@ public class Goal extends ParseObject{
 	public String getGoalDescription() {
 		return getString("goalDescription");
 	}
-
 }
