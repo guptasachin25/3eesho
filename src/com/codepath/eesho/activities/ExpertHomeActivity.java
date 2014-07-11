435b330b5394b04df4a326552bcbcec978c14499
caren <cchang@tagged.com>
2014-07-10 23:42:06 -0700

    adding ability for experts to edit tasks

--- src/com/codepath/eesho/activities/ExpertHomeActivity.java
+++ src/com/codepath/eesho/activities/ExpertHomeActivity.java
@@ -7,33 +7,30 @@ import android.view.View;
 
 import com.codepath.eesho.R;
 import com.codepath.eesho.fragments.ClientListFragment;
+import com.codepath.eesho.fragments.EditPlanActivity;
 
 public class ExpertHomeActivity extends FragmentActivity {
-	
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_expert_home);
-		
+
 		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
 		ft.replace(R.id.flPlaceholder, new ClientListFragment());
 		ft.commit();
 
 	}
-	
+
 	public void goToMessages() {
-		
+
 	}
-	
+
 	public void goToEditPlan(View v) {
-//		Intent i = new Intent(ExpertHomeActivity.this, EditPlanActivity.class);
-//        startActivity(i);
 		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-		// Replace the container with the new fragment
 		ft.replace(R.id.flPlaceholder, new EditPlanActivity());
-		// or ft.add(R.id.your_placeholder, new FooFragment());
-		// Execute the changes specified
+		ft.addToBackStack("tag");
 		ft.commit();
 	}
 }
