17181e6aba97b3407007e48b739e373d1a5f6028
gupta.sachin25 <gupta.sachin25@gmail.com>
2014-07-16 12:56:06 -0700

    Adding wallfragment

---
+++ src/com/codepath/eesho/adapters/MessagesAdapter.java
@@ -0,0 +1,41 @@
+package com.codepath.eesho.adapters;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.codepath.eesho.R;
+import com.codepath.eesho.parse.models.Messages;
+
+public class MessagesAdapter extends ArrayAdapter<Messages>{
+	public static Messages message;
+	
+	public View getView(int position, View convertView, ViewGroup parent) {
+		message = getItem(position);
+		View v;
+		
+		if (convertView == null) {
+			LayoutInflater inflator = LayoutInflater.from(getContext());
+			v = inflator.inflate(R.layout.messages_item, parent, false);
+		} else {
+			v = convertView;
+		}
+		
+		TextView tvDescription = (TextView) v.findViewById(R.id.tvDescription);
+		TextView timestamp = (TextView) v.findViewById(R.id.tvTimespan);
+		
+		tvDescription.setText(message.getMessage());	
+		System.out.println(message.getTimestamp());
+		timestamp.setText(message.getTimeSinceCurrentTime(message.getTimestamp()));
+		return v;
+	}
+	
+	public MessagesAdapter(Context context, ArrayList<Messages> messages) {
+		super(context, 0, messages);
+	}
+}
