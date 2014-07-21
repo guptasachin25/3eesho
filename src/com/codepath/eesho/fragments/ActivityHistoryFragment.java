21a25992340f03f36224a74ea803120e685872f3
caren <cchang@tagged.com>
2014-07-20 22:12:21 -0700

    adding history fragment stuff

--- src/com/codepath/eesho/fragments/ActivityHistoryFragment.java
+++ src/com/codepath/eesho/fragments/ActivityHistoryFragment.java
@@ -1,6 +1,5 @@
 package com.codepath.eesho.fragments;
 
-import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,10 +8,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.codepath.eesho.R;
 
 public class ActivityHistoryFragment extends Fragment{
+	
+	ProgressBar pbMiles;
+	ProgressBar pbSitups;
+	ProgressBar pbCrunches;
+	ProgressBar pbPushups;
+	ProgressBar pbLunges;
+	ProgressBar pbShoulder;
+	
+	TextView tvMiles;
+	TextView tvSitups;
+	TextView tvCrunches;
+	TextView tvPushups;
+	TextView tvLunges;
+	TextView tvShoulder;
 	
 	Button bWeek;
 	Button bMonth;
@@ -31,13 +45,21 @@ public class ActivityHistoryFragment extends Fragment{
 		bMonth = (Button) v.findViewById(R.id.btMonth);
 		bAll = (Button) v.findViewById(R.id.btAll);
 		
-		ProgressBar pb = (ProgressBar) v.findViewById(R.id.pbCrunches);
-		pb.setMax(100);
-		pb.setProgress(50);
+		pbMiles = (ProgressBar) v.findViewById(R.id.pbMiles);
+		pbSitups = (ProgressBar) v.findViewById(R.id.pbSitups);
+		pbCrunches = (ProgressBar) v.findViewById(R.id.pbCrunches);
+		pbPushups = (ProgressBar) v.findViewById(R.id.pbPushups);
+		pbLunges = (ProgressBar) v.findViewById(R.id.pbLunges);
+		pbShoulder = (ProgressBar) v.findViewById(R.id.pbShoulder);
 		
-		ProgressBar pbPushups = (ProgressBar) v.findViewById(R.id.pbPushups);
-		pbPushups.setMax(100);
-		pbPushups.setProgress(30);
+		tvMiles = (TextView) v.findViewById(R.id.tvMilesCount);
+		tvSitups = (TextView) v.findViewById(R.id.tvSitupsCount);
+		tvCrunches = (TextView) v.findViewById(R.id.tvCrunchesCount);
+		tvPushups = (TextView) v.findViewById(R.id.tvPushupsCount);
+		tvLunges = (TextView) v.findViewById(R.id.tvLungesCount);
+		tvShoulder = (TextView) v.findViewById(R.id.tvShoulderCount);
+
+		setUpWeekBars();
 		
 		bMonth.setOnClickListener(new View.OnClickListener() {
 		    @Override
@@ -50,7 +72,7 @@ public class ActivityHistoryFragment extends Fragment{
 		        	setToUnclick(bAll);
 		        }
 		        currentlyClickedBtn = 2;
-		        
+		        setUpMonthBars();
 		    }
 		});
 		
@@ -65,7 +87,7 @@ public class ActivityHistoryFragment extends Fragment{
 		        	setToUnclick(bMonth);
 		        }
 		        currentlyClickedBtn = 3;
-		        
+		        setUpAllBars();
 		    }
 		});
 		
@@ -80,7 +102,7 @@ public class ActivityHistoryFragment extends Fragment{
 		        	setToUnclick(bAll);
 		        }
 		        currentlyClickedBtn = 1;
-		        
+		        setUpWeekBars();
 		    }
 		});
 		
@@ -95,6 +117,41 @@ public class ActivityHistoryFragment extends Fragment{
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+	}
+	
+	public void setBar (ProgressBar b, TextView tv, int max, int progress) {
+		b.setMax(max);
+		b.setProgress(progress);
+		tv.setText(Integer.toString(progress));
+	}
+	
+	public void setUpWeekBars() {
+		
+		setBar(pbMiles, tvMiles, 25, 50);
+		setBar(pbSitups, tvSitups, 100, 30);
+		setBar(pbCrunches, tvCrunches, 200, 34);
+		setBar(pbPushups, tvPushups, 500, 200);
+		setBar(pbLunges, tvLunges, 500, 125);
+		setBar(pbShoulder, tvShoulder, 300, 175);
+		
+	}
+	
+	public void setUpMonthBars() {
+		setBar(pbMiles, tvMiles, 50, 50);
+		setBar(pbSitups, tvSitups, 400, 30);
+		setBar(pbCrunches, tvCrunches, 300, 34);
+		setBar(pbPushups, tvPushups, 500, 200);
+		setBar(pbLunges, tvLunges, 900, 125);
+		setBar(pbShoulder, tvShoulder, 300, 175);
+	}
+	
+	public void setUpAllBars() {
+		setBar(pbMiles, tvMiles, 500, 50);
+		setBar(pbSitups, tvSitups, 10000, 30);
+		setBar(pbCrunches, tvCrunches, 20000, 34);
+		setBar(pbPushups, tvPushups, 50000, 200);
+		setBar(pbLunges, tvLunges, 50000, 125);
+		setBar(pbShoulder, tvShoulder, 10000, 175);	
 	}
 
 }
