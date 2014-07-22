c8b1db729c730ebc82da633120a3751f69b420dc
caren <cchang@tagged.com>
2014-07-22 01:33:22 -0700

    removing shout button in menu

--- src/com/codepath/eesho/activities/HomeActivity.java
+++ src/com/codepath/eesho/activities/HomeActivity.java
@@ -95,66 +95,6 @@ public class HomeActivity extends FragmentActivity {
 		getPlanView();
 	}
 
-	@SuppressWarnings("deprecation")
-	public void onShout(MenuItem mi) {
-		Toast.makeText(getApplicationContext(), "Shout!!!", Toast.LENGTH_SHORT).show();
-		DateTime date = new DateTime();
-		ParseQuery<Goal> query = ParseQuery.getQuery(Goal.class);
-		query.whereEqualTo("user", ParseUser.getCurrentUser());
-		query.whereEqualTo("date", date.toDateMidnight().toDate());
-
-		query.getFirstInBackground(new GetCallback<Goal>() {
-			public void done(Goal goal, ParseException e) {
-				if (e == null) {
-					try {
-						StringBuilder builder = new StringBuilder();
-						builder.append("<font color='#01DFD7'><b>" + ParseUser.getCurrentUser().getString("name") + 
-								"</b></font>" +" did ");
-						boolean flag = false;
-						for(FitnessPlanSingleActivity activity: goal.getDailyActivity().getActivityList()) {
-							if(activity.isDone()) {
-								if(activity.getDescription().toLowerCase().contains("run")) {
-									Messages message = new Messages();
-									message.setMessage(String.format("<font color='#01DFD7'><b>%s</b></font> ran %d miles", 
-											ParseUser.getCurrentUser().getString("name"), activity.getDistance()));
-
-									message.setSender(ParseUser.getCurrentUser());
-									message.saveInBackground();
-									continue;
-								} else if(activity.getDescription().toLowerCase().contains("walk")) {
-									Messages message = new Messages();
-									message.setMessage(String.format("<font color='#01DFD7'><b>%s</b></font> walked for %d minutes", 
-											ParseUser.getCurrentUser().getString("name"), activity.getDuration()));
-									message.setSender(ParseUser.getCurrentUser());
-									message.saveInBackground();
-									continue;
-								} else {
-									flag = true;
-									builder.append(activity.getRepititions() * activity.getSets() + " " + 
-											activity.getDescription() + ", ");
-								}
-							}
-						}
-						
-						if(flag) {
-							Messages message = new Messages();
-							String m = builder.toString().trim();
-							message.setMessage(m.substring(0, m.length()-1) + ".");
-							message.setSender(ParseUser.getCurrentUser());
-							message.saveInBackground();
-						}
-
-					} catch (JSONException e1) {
-						e1.printStackTrace();
-					}
-				} else {
-					Log.d("item", "Error: " + e.getMessage());
-				}
-			}
-		});
-		onSocialView(null);
-	}
-
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -168,63 +108,6 @@ public class HomeActivity extends FragmentActivity {
 		fts.addToBackStack(null);
 		fts.commit();
 	}
-	
-
-	private void setupTabs() {
-		ActionBar actionBar = getActionBar();
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		actionBar.setDisplayShowTitleEnabled(true);
-		/*
-		Tab myDashboard = actionBar
-				.newTab()
-				.setText("MyDashBoard")
-				//.setIcon(R.drawable.ic_mentions)
-				.setTag("MyDashBoard")
-				.setTabListener(
-				    new FragmentTabListener<UserDashBoardFragment>(R.id.flHomeContainer, this, "MyDashboard",
-				    		UserDashBoardFragment.class));
-		 */
-		/*
-		Tab myTrainer = actionBar
-				.newTab()
-				.setText("MyTrainer")
-				//.setIcon(R.drawable.ic_mentions)
-				.setTag("MyTrainerFragment")
-				.setTabListener(
-				    new FragmentTabListener<MyTrainerFragment>(R.id.flHomeContainer, this, "MyTrainer",
-				    		MyTrainerFragment.class));
-		 */
-
-		Tab myPlan = actionBar
-				.newTab()
-				.setText("My Plan")
-				//.setIcon(R.drawable.ic_home)
-				.setTag("DailyPlanFragment")
-				.setTabListener(
-						new FragmentTabListener<UserDashBoardFragment>(R.id.flHomeContainer, this, "MyPlan",
-								UserDashBoardFragment.class));
-		/*
-		Tab myArticle = actionBar
-				.newTab()
-				.setText("MyArticle")
-				//.setIcon(R.drawable.ic_mentions)
-				.setTag("MyArticleFragment")
-				.setTabListener(
-				    new FragmentTabListener<ArticleFragment>(R.id.flHomeContainer, this, "MyArticle",
-				    		ArticleFragment.class));
-		 */
-		Tab myWall = actionBar.newTab()
-				.setText("Social")
-				.setTag("MyWallFragment")
-				.setTabListener(new FragmentTabListener<WallFragment>(R.id.flHomeContainer, this, "MyWall", WallFragment.class));
-
-		actionBar.addTab(myPlan);
-		//actionBar.addTab(myDashboard);
-		//actionBar.addTab(myTrainer);
-		//actionBar.addTab(myArticle);
-		actionBar.addTab(myWall);
-		actionBar.selectTab(myPlan);
-        }
 
 	public void openProgress(View v) {
 		FragmentTransaction fts = getSupportFragmentManager().beginTransaction();
