c05fa7bdd402e1ecc412e821e1242931997df3c4
caren <cchang@tagged.com>
2014-07-22 01:20:28 -0700

    styling changes for progress fragment, styling changes for history fragment, hooking up history fragment to parse data

--- src/com/codepath/eesho/fragments/UserDashBoardFragment.java
+++ src/com/codepath/eesho/fragments/UserDashBoardFragment.java
@@ -7,8 +7,10 @@ import java.util.Random;
 import org.joda.time.DateTime;
 import org.json.JSONException;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Path;
 import android.graphics.Rect;
 import android.os.Bundle;
@@ -16,19 +18,23 @@ import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ImageView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.codepath.eesho.R;
+import com.codepath.eesho.activities.ShoutActivity;
 import com.codepath.eesho.adapters.GoalArrayAdapter;
 import com.codepath.eesho.models.DailyActivity;
 import com.codepath.eesho.models.FitnessPlanSingleActivity;
 import com.codepath.eesho.models.SingleActivity;
 import com.codepath.eesho.parse.models.Goal;
+import com.codepath.eesho.parse.models.Messages;
 import com.codepath.eesho.parse.models.MyActivity;
 import com.facebook.widget.ProfilePictureView;
 import com.parse.GetCallback;
@@ -41,6 +47,7 @@ public class UserDashBoardFragment extends Fragment {
 
 	private ArrayList<SingleActivity> goals;
 	private GoalArrayAdapter aGoals;
+	Button btShout;
 	ListView lvGoals;
 	int fragmentNumber;
 	DailyActivity<FitnessPlanSingleActivity> dailyActivity = null;
@@ -51,6 +58,16 @@ public class UserDashBoardFragment extends Fragment {
 
 	private String getUserName(ParseUser user) {
 		return user.getString("name");
+	}
+	
+	public int getDoneGoals() {
+		int doneCount = 0;
+		for (SingleActivity s : goals) {
+			if (s.isDone()) {
+				doneCount ++;
+			}
+		}
+		return doneCount;
 	}
 	
 	private String getUserTarget(ParseUser user) {
@@ -116,6 +133,7 @@ public class UserDashBoardFragment extends Fragment {
 		tvName = (TextView) v.findViewById(R.id.tvName);
 		tvGoal = (TextView) v.findViewById(R.id.tvGoal);
 		tvActivity = (TextView) v.findViewById(R.id.tvActivity);
+		btShout = (Button) v.findViewById(R.id.btShout);
 
 		ivPicture.setPresetSize(ProfilePictureView.NORMAL);
 		String facebookPic = ParseUser.getCurrentUser().getString("facebook_id");
@@ -128,13 +146,31 @@ public class UserDashBoardFragment extends Fragment {
 
 		tvName.setText(getUserName(ParseUser.getCurrentUser()));
 		tvGoal.setText(getUserTarget(ParseUser.getCurrentUser()));
+		
+		// this doesnt work??
+		changeShoutButton(getDoneGoals());
+		
+		btShout.setOnClickListener(new OnClickListener(){
+	        @Override
+	        public void onClick(View view) {
+	    		int numOfGoals = goals.size();
+	    		int doneCount = getDoneGoals();
+	    		if (doneCount == numOfGoals) {
+	    			Toast.makeText(getActivity(), "Awesome job!", Toast.LENGTH_SHORT).show();
+	    			shout();
+	    		} else if (doneCount == numOfGoals - 1) {
+	    			Toast.makeText(getActivity(), "You're almost there!", Toast.LENGTH_SHORT).show();
+	    		} else {
+	    			Toast.makeText(getActivity(), "Do some more exercise!", Toast.LENGTH_SHORT).show();
+	    		}
+	        }
+	    });
 
 		lvGoals.setOnItemClickListener(new OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> parent, final View view,
 					int position, long id) {
 				final Boolean done;
-				System.out.println("I came here after click");
 				try {
 					System.out.println(goals.get(position));
 					System.out.println(((FitnessPlanSingleActivity) view.getTag()).toJSONObject());
@@ -170,6 +206,7 @@ public class UserDashBoardFragment extends Fragment {
 							myActivity.saveInBackground();
 							tvActivity.setText(
 									Long.toString(Long.parseLong(tvActivity.getText().toString()) + steps));
+							changeShoutButton(getDoneGoals());
 						}
 					}
 				});
@@ -177,6 +214,19 @@ public class UserDashBoardFragment extends Fragment {
 			}
 		});
 		return v;
+	}
+	
+	public void changeShoutButton(int num) {
+		switch (num) {
+			case 1: btShout.setBackgroundResource(R.drawable.shout_button_1);
+				break;
+        	case 2: btShout.setBackgroundResource(R.drawable.shout_button_2);
+                break;
+        	case 3:btShout.setBackgroundResource(R.drawable.shout_button);
+        		break;
+        	case 0: btShout.setBackgroundResource(R.drawable.shout_button_0);
+    			break;
+		}
 	}
 
 	public Bitmap getRoundedShape(Bitmap scaleBitmapImage) {
@@ -200,5 +250,65 @@ public class UserDashBoardFragment extends Fragment {
 						sourceBitmap.getHeight()), 
 						new Rect(0, 0, targetWidth, targetHeight), null);
 		return targetBitmap;
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void shout() {
+		DateTime date = new DateTime();
+		ParseQuery<Goal> query = ParseQuery.getQuery(Goal.class);
+		query.whereEqualTo("user", ParseUser.getCurrentUser());
+		query.whereEqualTo("date", date.toDateMidnight().toDate());
+
+		query.getFirstInBackground(new GetCallback<Goal>() {
+			public void done(Goal goal, ParseException e) {
+				if (e == null) {
+					try {
+						StringBuilder builder = new StringBuilder();
+						builder.append("<font color='#01DFD7'><b>" + ParseUser.getCurrentUser().getString("name") + 
+								"</b></font>" +" did ");
+						boolean flag = false;
+						for(FitnessPlanSingleActivity activity: goal.getDailyActivity().getActivityList()) {
+							if(activity.isDone()) {
+								if(activity.getDescription().toLowerCase().contains("run")) {
+									Messages message = new Messages();
+									message.setMessage(String.format("<font color='#01DFD7'><b>%s</b></font> ran %d miles", 
+											ParseUser.getCurrentUser().getString("name"), activity.getDistance()));
+
+									message.setSender(ParseUser.getCurrentUser());
+									message.saveInBackground();
+									continue;
+								} else if(activity.getDescription().toLowerCase().contains("walk")) {
+									Messages message = new Messages();
+									message.setMessage(String.format("<font color='#01DFD7'><b>%s</b></font> walked for %d minutes", 
+											ParseUser.getCurrentUser().getString("name"), activity.getDuration()));
+									message.setSender(ParseUser.getCurrentUser());
+									message.saveInBackground();
+									continue;
+								} else {
+									flag = true;
+									builder.append(activity.getRepititions() * activity.getSets() + " " + 
+											activity.getDescription() + ", ");
+								}
+							}
+						}
+						
+						if(flag) {
+							Messages message = new Messages();
+							String m = builder.toString().trim();
+							message.setMessage(m.substring(0, m.length()-1) + ".");
+							message.setSender(ParseUser.getCurrentUser());
+							message.saveInBackground();
+						}
+
+					} catch (JSONException e1) {
+						e1.printStackTrace();
+					}
+				} else {
+					Log.d("item", "Error: " + e.getMessage());
+				}
+			}
+		});
+		Intent intent = new Intent(this.getActivity(), ShoutActivity.class);
+		startActivity(intent);
 	}
 }
